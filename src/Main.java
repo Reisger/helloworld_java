0b9929d0719a1997f09316436d84e2589010546d
Gerciana Reis <gercianareisdesouza@gmail.com>
2022-08-10 11:38:19 -0400

    add classe student

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Java is working!!!!!!!!");
+
+        Student student1 = new Student("Gerciana", 24);
+
+        System.out.println(student1);
+
+        System.out.println(student1.getAge());
+
+        System.out.println(student1.getName());
+
     }
 }
